27273a31943aae1057c72e72befeec75fa420c9b
omisso <andresafira2004@gmail.com>
2024-12-09 14:20:02 -0300

    feat: implement simulation and main module for testing

--- main_playable.py
+++ main_playable.py
@@ -1,4 +1,4 @@
-from constants import FREQUENCY, CAR_MAX_SPEED
+from constants import FREQUENCY
 from simulation import Simulation
 import pygame
 import sys
@@ -7,8 +7,7 @@ import sys
 def main():
     """Performs a simulation controlled by the user, so the car dynamics can be
     tested and explored"""
-    sim = Simulation(side='right', draw_Bounding_Box=True, draw_reference_line=False, draw_car_line=False)
-    sim.dummy_simple_generator(num_dummy = 10, step = 700, side='right', speed = CAR_MAX_SPEED/3)
+    sim = Simulation(draw_reference_line=False, draw_rocket_line=False)
     run = True
 
     while run:
@@ -22,16 +21,15 @@ def main():
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_w] or keys[pygame.K_UP]:
-            sim.car.accelerate()
+            sim.rocket.increaseThrust()
         if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-            sim.car.turn_left()
+            sim.rocket.turnLeft()
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-            sim.car.turn_right()
+            sim.rocket.turnRight()
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-            sim.car.brake()
+            sim.rocket.decreaseThrust()
         if keys[pygame.K_r]:
-            sim.reset(side="right")
-            sim.dummy_simple_generator(10, step = 700, side='right', speed = CAR_MAX_SPEED/3)
+            sim.reset()
 
 if __name__ == "__main__":
     main()
--- simulation.py
+++ simulation.py
@@ -1,198 +1,108 @@
-from constants import CAR_START_LEFT, CAR_START_RIGHT, CAR_MAX_SPEED, SAFETY_DISTANCE_BACK, SAFETY_DISTANCE_FRONT
-from constants import WIDTH, HEIGHT, CAR_HEIGHT, CAR_WIDTH, BACKGROUND_SPRITE, CAR_SPRITE
-from constants import SAMPLE_TIME, eps, SIDEWALK_WIDTH, MIDDLE_RIGHT, MIDDLE_LEFT
-from constants import BLACK, WHITE, RED
+from constants import D_TIME, BLACK, WHITE, RED, M2P, WIDTH, HEIGHT
+from constants import ROCKET_WIDTH, ROCKET_HEIGHT, FIRE_WIDTH, FIRE_HEIGHT
+from constants import ROCKET_SPRITE, BACKGROUND_SPRITE, FIRE_SPRITE, MAX_THRUST
+from rocket import Rocket
 
-from Utils.Geometry.Position import Position
-from Utils.General import clip
-from Utils.Geometry.Box import *
+from utils import clip
 
 import pygame
-from pygame.transform import scale, rotate
+from pygame.transform import scale, rotate, flip
 from pygame.image import load
 from pygame.locals import *
 
 from math import pi, sin, cos, fabs
-from Car import Car
-
-
-def make_default_position(location):
-    return Position(Vector(location[0], location[1]), 0)
+from rocket import Rocket
 
 
 class Simulation:
     """Main simulation engine that manages the interaction between objects"""
-    def __init__(self, side: str, draw_Bounding_Box: bool, draw_reference_line: bool, draw_car_line: bool):
+    def __init__(self, draw_reference_line: bool = False, draw_rocket_line: bool = False):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.screen = pygame.display.set_mode((WIDTH*M2P, HEIGHT*M2P))
         pygame.display.set_caption("Control System Simulation")
-        self.background_sprite = load(BACKGROUND_SPRITE)
-        self.car_sprite = scale(load(CAR_SPRITE), (CAR_WIDTH*1.1, CAR_HEIGHT))
-
-        if side == 'left':
-            self.car = Car(make_default_position(CAR_START_LEFT))
-        elif side == 'right':
-            self.car = Car(make_default_position(CAR_START_RIGHT))
-        else:
-            raise Exception('Invalid side choice! Choose left or right side')
-
-        self.dummies = [] # stores the dummy cars
-        self.objects = [] # stores the objects in the simulation
+        self.background_sprite = scale(load(BACKGROUND_SPRITE), (WIDTH*M2P, HEIGHT*M2P))
+        self.rocket_sprite = scale(load(ROCKET_SPRITE), (ROCKET_WIDTH*M2P, ROCKET_HEIGHT*M2P))
+        self.fire_sprite = flip(scale(load(FIRE_SPRITE), (FIRE_WIDTH*M2P, FIRE_HEIGHT*M2P)), False, True)
+        
+        self.rocket = Rocket(locX=WIDTH/2)
 
         self.reference_points = [] # reference points (xr) to show on screen 
-        self.max_rp = 100 # maximum number of reference points stored
-        self.car_points = [] # actual car position (x) to show on screen
-        self.max_cp = 100 # maximum number of points stored
+        self.max_ref = 100 # maximum number of reference points stored
+        self.rocket_points = [] # actual rocket position (x) to show on screen
+        self.max_roc = 100 # maximum number of points stored
 
-        self.draw_BB = draw_Bounding_Box
-        self.draw_RL = draw_reference_line
-        self.draw_CL = draw_car_line
+        self.draw_ref = draw_reference_line
+        self.draw_roc = draw_rocket_line
 
         self.update_check = 0
 
-    def dummy_simple_generator(self, num_dummy, step, side, speed):
-        """Method that generates an alternating lanes set of dummy cars with
-        specified number, speed, initial side and distance between."""
-
-        if side == 'left':
-            first = MIDDLE_RIGHT
-            second = MIDDLE_LEFT
-        elif side == 'right':
-            first = MIDDLE_LEFT
-            second = MIDDLE_RIGHT
-        else:
-            raise Exception('Invalid side choice! Choose left or right side')
-
-        self.dummies.clear()
-        for i in range(num_dummy):
-            if i % 2 == 0:
-                x = first
-            else:
-                x = second
-            pose = Position(Vector(x, HEIGHT/2 - CAR_HEIGHT + i * step), 0)
-            self.dummies.append(Car(initial_position=pose, DUMMY=True, initial_speed=speed))
-    
-    def reset(self, side: str):
-        self.car.speed = 0
-        self.car.alive = True
-        self.dummies.clear()
-        self.update_check = 0
-        if side == 'left':
-            self.car.position = make_default_position(CAR_START_LEFT)
-        elif side == 'right':
-            self.car.position = make_default_position(CAR_START_RIGHT)
-        else:
-            raise Exception('Invalid side choice! Choose between left or right side')
-        self.update_objects()
+    def reset(self):
+        self.rocket = Rocket(locX=WIDTH/2)
 
     def get_reference_parameters(self) -> tuple[float, float]:
         """Function that returns the values of vr and yr (reference values for
         the control system to follow)"""
-        vr = CAR_MAX_SPEED
-        xr = MIDDLE_RIGHT
-
-        # If there is a dummy car ahead (given a safety threshold) the reference point goes
-        # to the left lane, and, after it passes over (given another safety threshold) the 
-        # reference point goes back to the right lane
-        for dummy in self.dummies:
-            delta_y = dummy.position.location.y - self.car.position.location.y
-            if delta_y > 0 and delta_y < SAFETY_DISTANCE_BACK or delta_y < 0 and -delta_y < SAFETY_DISTANCE_FRONT:
-                xr = MIDDLE_LEFT
-                break
+        vr: float = 0.0
+        zr: float = 0.0
         
-        if (self.draw_RL):
-            self.reference_points.append((xr, self.car.position.location.y))
-            if len(self.reference_points) > self.max_rp:
+        if self.draw_ref:
+            self.reference_points.append((zr, self.rocket.locZ))
+            if len(self.reference_points) > self.max_ref:
                 del self.reference_points[0]
-        return vr, xr
+        return vr, zr
 
-    def generate_dummy(self, x_position, speed):
-        pose = Position(Vector(x_position, self.car.position.location.y + CAR_HEIGHT/2 + HEIGHT/2), 0)
-        car = Car(initial_position=pose, DUMMY=True, initial_speed=speed)
-        self.dummies.append(car)
+    def draw_object(self, sprite, pos: tuple):
+        anchored_pos = (pos[0], HEIGHT*M2P/2 + pos[1] - self.rocket.locZ*M2P)
+        self.screen.blit(sprite, anchored_pos)
 
-    def update_objects(self):
-        self.objects.clear()
-        for dummy in self.dummies:
-            dummy.update()
-
-        # ROAD Segment:
-        left_side = Segment(Vector(SIDEWALK_WIDTH, self.car.position.location.y - CAR_HEIGHT),
-                            Vector(SIDEWALK_WIDTH, self.car.position.location.y + CAR_HEIGHT))
-        right_side = Segment(Vector(WIDTH - SIDEWALK_WIDTH, self.car.position.location.y - CAR_HEIGHT),
-                             Vector(WIDTH - SIDEWALK_WIDTH, self.car.position.location.y + CAR_HEIGHT))
-
-        self.objects.append(left_side)
-        self.objects.append(right_side)
-        for dummy in self.dummies:
-            self.objects.append(dummy.bounding_box)
-
-    def draw_bounding_box(self, car: Car):
-        box = car.unravel_box()
-        P1 = box.vertices[0].x, HEIGHT/2 - box.vertices[0].y + self.car.position.location.y + CAR_HEIGHT / 2
-        P2 = box.vertices[1].x, HEIGHT/2 - box.vertices[1].y + self.car.position.location.y + CAR_HEIGHT / 2
-        P3 = box.vertices[2].x, HEIGHT/2 - box.vertices[2].y + self.car.position.location.y + CAR_HEIGHT / 2
-        P4 = box.vertices[3].x, HEIGHT/2 - box.vertices[3].y + self.car.position.location.y + CAR_HEIGHT / 2
-
-        pygame.draw.line(self.screen, WHITE, P1, P2)
-        pygame.draw.line(self.screen, WHITE, P2, P3)
-        pygame.draw.line(self.screen, WHITE, P3, P4)
-        pygame.draw.line(self.screen, WHITE, P4, P1)
-
-    def draw_car(self, car: Car):
-        sprite_copy = rotate(self.car_sprite, -car.position.rotation * 180 / pi)
-        if not self.car.alive:
-            sprite_copy.set_alpha(50)
-        self.screen.blit(sprite_copy, (car.position.location.x - sprite_copy.get_width() / 2,
-                                       HEIGHT / 2 + CAR_HEIGHT/2 - sprite_copy.get_height() / 2 - car.position.location.y + self.car.position.location.y))
-        if self.draw_BB:
-            self.draw_bounding_box(car)
+    def draw_rocket(self):
+        fire_rot = self.rocket.theta + 2*self.rocket.nozzleAngle
+        fire_height = FIRE_HEIGHT * self.rocket.thrust / MAX_THRUST
+        fire = rotate(scale(self.fire_sprite, (FIRE_WIDTH*M2P, fire_height * M2P)), -fire_rot * 180 / pi)
+        
+        theta = self.rocket.theta
+        anchor_pos = (self.rocket.locX*M2P - 0.95 * ROCKET_HEIGHT/2 * M2P * sin(theta) - FIRE_WIDTH/2,
+                      self.rocket.locZ*M2P + 0.95 * ROCKET_HEIGHT/2 * M2P * cos(theta))
+        final_pos = (anchor_pos[0] - fire_height * M2P / 2 * sin(fire_rot) - fire.get_width()/2,
+                     anchor_pos[1] + fire_height * M2P / 2 * cos(fire_rot) - fire.get_height()/2)
+        self.draw_object(fire, final_pos)
+        rocket = rotate(self.rocket_sprite, -self.rocket.theta * 180 / pi)
+        pos = (self.rocket.locX*M2P - rocket.get_width() / 2,
+               self.rocket.locZ*M2P - rocket.get_height() / 2)
+        self.draw_object(rocket, pos)
 
     def draw_scenario(self):
-        self.screen.blit(self.background_sprite, (0, clip(self.car.position.location.y, HEIGHT)))
-        self.screen.blit(self.background_sprite, (0, clip(self.car.position.location.y - HEIGHT, -HEIGHT)))
-        for dummy in self.dummies:
-            self.draw_car(dummy)
+        self.screen.blit(self.background_sprite, (0, clip(self.rocket.locZ*M2P, HEIGHT*M2P)))
+        self.screen.blit(self.background_sprite, (0, clip(self.rocket.locZ*M2P - HEIGHT*M2P, -HEIGHT*M2P)))
         
-        if self.draw_RL:
+        if self.draw_ref:
             for i in range(1, len(self.reference_points)):
-                P1 = self.reference_points[i-1]
-                P1 = P1[0], HEIGHT/2 - P1[1] + self.car.position.location.y
-                P2 = self.reference_points[i]
-                P2 = P2[0], HEIGHT/2 - P2[1] + self.car.position.location.y
+                P1 = self.reference_points[i-1] * M2P
+                P1 = P1[0], HEIGHT*M2P/2 - P1[1] + self.rocket.locZ * M2P
+                P2 = self.reference_points[i] * M2P
+                P2 = P2[0], HEIGHT*M2P/2 - P2[1] + self.rocket.locZ * M2P
                 pygame.draw.line(self.screen, RED, P1, P2)
 
-        if self.draw_CL:
-            for i in range(1, len(self.car_points)):
-                P1 = self.car_points[i-1]
-                P1 = P1[0], HEIGHT/2 - P1[1] + self.car.position.location.y
-                P2 = self.car_points[i]
-                P2 = P2[0], HEIGHT/2 - P2[1] + self.car.position.location.y
+        if self.draw_roc:
+            for i in range(1, len(self.rocket_points)):
+                P1 = self.rocket_points[i-1] * M2P
+                P1 = P1[0], HEIGHT*M2P/2 - P1[1] + self.rocket.locZ * M2P
+                P2 = self.rocket_points[i] * M2P
+                P2 = P2[0], HEIGHT*M2P/2 - P2[1] + self.rocket.locZ * M2P
                 pygame.draw.line(self.screen, WHITE, P1, P2)
 
-        self.draw_car(self.car)
+        self.draw_rocket()
 
     def update(self):
         self.screen.fill(BLACK)
-        if self.update_check != 0:
-            self.objects.clear()
-            self.update_check = (self.update_check + 1) % 3
-
-        # Removing cars that are behind
-        i = 0
-        while i < len(self.dummies):
-            if self.car.position.location.y - self.dummies[i].position.location.y > HEIGHT:
-                del self.dummies[i]
-            else:
-                i += 1
-
-        self.car.update(self.objects)
-        if (self.draw_CL):
-            self.car_points.append((self.car.position.location.x, self.car.position.location.y))
-            if len(self.car_points) > self.max_cp:
-                del self.car_points[0]
+        if self.draw_roc:
+            self.rocket_points.append((self.rocket.locX, self.rocket.locZ))
+            if len(self.rocket_points) > self.max_roc:
+                del self.rocket_points[0]
 
         self.draw_scenario()
-        self.update_objects()
+        self.rocket.move(0, 0)
+        if self.rocket.locX < 0 or self.rocket.locX > WIDTH:
+            self.reset()
         pygame.display.flip()
